2068a798e70652553df2637e738036924ce23908
➳SΞHΞЯ <57824894+seherkaushal@users.noreply.github.com>
2020-04-14 17:53:47 +0530

    Add files via upload

---
+++ Snake zipped/sketch.js
@@ -0,0 +1,63 @@
+
+
+let snake;
+let rez = 40;
+let food;
+let w;
+let h;
+
+function setup() {
+  createCanvas(1350,780);
+  w = floor(width / rez);
+  h = floor(height / rez);
+  frameRate(5);
+  snake = new Snake();
+  foodLocation();
+}
+
+function preload() {
+  backgroundImg = loadImage("sprites/cute.jpg");
+}
+
+function foodLocation() {
+  let x = floor(random(w));
+  let y = floor(random(h));
+  food = createVector(x, y);
+
+}
+
+function keyPressed() {
+  if (keyCode === LEFT_ARROW) {
+    snake.setDir(-1, 0);
+  } else if (keyCode === RIGHT_ARROW) {
+    snake.setDir(1, 0);
+  } else if (keyCode === DOWN_ARROW) {
+    snake.setDir(0, 1);
+  } else if (keyCode === UP_ARROW) {
+    snake.setDir(0, -1);
+  } else if (key == ' ') {
+    snake.grow();
+  }
+
+}
+
+function draw() {
+  scale(rez);
+  background(backgroundImg);
+  if (snake.eat(food)) {
+    foodLocation();
+  }
+  snake.update();
+  snake.show();
+
+
+  if (snake.endGame()) {
+    print("END GAME");
+    background(255, 0, 0);
+    noLoop();
+  }
+
+  noStroke();
+  fill(255, 0, 0);
+  rect(food.x, food.y, 1, 1);
+}
